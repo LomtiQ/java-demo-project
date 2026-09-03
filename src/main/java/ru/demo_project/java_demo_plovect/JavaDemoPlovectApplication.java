5957625aa923acd3aeaad7300d455572bfe5e6d6
nikitalomov <n.lomtiq@gmail.com>
2026-09-03 17:24:54 +0300

    new file:   .gitignore 	new file:   .idea/.gitignore 	new file:   .idea/amplicode-jpa.xml 	new file:   .idea/misc.xml 	new file:   .idea/vcs.xml 	new file:   pom.xml 	new file:   src/main/java/ru/demo_project/java_demo_plovect/JavaDemoPlovectApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/ru/demo_project/java_demo_plovect/JavaDemoPlovectApplicationTests.java
    	modified:   pom.xml
    	modified:   src/main/java/ru/demo_project/java_demo_plovect/JavaDemoPlovectApplication.java
    	modified:   src/test/java/ru/demo_project/java_demo_plovect/JavaDemoPlovectApplicationTests.java

---
+++ src/main/java/ru/demo_project/java_demo_plovect/JavaDemoPlovectApplication.java
@@ -0,0 +1,13 @@
+package ru.demo_project.java_demo_plovect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDemoPlovectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDemoPlovectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/demo_project/java_demo_plovect/JavaDemoPlovectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.demo_project.java_demo_plovect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDemoPlovectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
